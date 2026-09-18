53718538d4833c51c692afe9bf8f731e57d11bbf
Vitor Luis <vitorlgv@proton.me>
2026-09-18 20:04:56 -0300

    Fix triangle validity condition logic

--- Faculdade/Faculdade/📁 2026.2/Coding/15-09/exe07.py
+++ Faculdade/Faculdade/📁 2026.2/Coding/15-09/exe07.py
@@ -2,7 +2,7 @@ lado1 = int(input("Digite o primeiro lado do triângulo: "))
 lado2 = int(input("Digite o segundo lado do triângulo: "))
 lado3 = int(input("Digite o terceiro lado do triângulo: "))
 
-t_valido = lado1 + lado2 > lado3 or lado3 + lado1 > lado2 or lado2 + lado3 > lado1
+t_valido = lado1 + lado2 > lado3 and lado3 + lado1 > lado2 and lado2 + lado3 > lado1
 
 if t_valido:
     if (lado1 == lado2 and lado2 == lado3):
